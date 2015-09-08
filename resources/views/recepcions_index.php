4a0d53e10b311fcbdd2cad940c9e46552d40d80a
e2fletcher <e2fletcher@gmail.com>
2015-09-08 11:57:55 -0430

    Rebase

---
+++ resources/views/recepcions_index.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('content')
+<div class="row">
+	<div class="col-md-12">
+		<div class="panel panel-default">
+			<div class="panel-heading">RECEPCIÓN DE PRODUCTOS</div>
+			<div class="panel-body">
+				
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+
+@section('body')
+<script>
+	$(document).ready(function(){
+	});
+</script>
+@endsection
