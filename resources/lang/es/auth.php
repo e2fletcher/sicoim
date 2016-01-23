16723b72330dc9404f06006b02bcde1fbac9368d
Ender Fletcher <e2fletcher@gmail.com>
2016-01-23 17:53:36 -0430

    Mejora de la traducción

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,5 +1,15 @@
 <?php
-
 return [
-    'failed' => 'Usuario o contraseña es incorrecto'
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
 ];
